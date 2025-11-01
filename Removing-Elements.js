84c0d66d320c6a4933d0211d6669631af0c8dbd3
MoamenAbouhaty95 <momenabohaty@gmail.com>
2025-11-01 19:41:21 +0300

    Initial commit

---
+++ Removing-Elements.js
@@ -0,0 +1,14 @@
+// Take an array and remove every second element from the array.
+// Always keep the first element and start removing with the next element.
+
+function removeEveryOther(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]));
